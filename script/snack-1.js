a023e7774164ee1c3841199b5775a7cd415d415a
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-28 16:33:56 +0100

    numero da 1 a 10

---
+++ script/snack-1.js
@@ -0,0 +1,8 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+let n = parseInt(prompt('Inserisci un numero compreso tra 1 e 10'));
+
+while (n < 1 || n > 10){
+    
+    let n = parseInt(prompt('Inserisci un numero compreso tra 1 e 10'));
+}
