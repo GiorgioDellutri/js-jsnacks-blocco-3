a0f95149b316ef1541dd1fa8fadba8a4ec64a4e4
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-28 16:41:55 +0100

    rev

--- script/snack-1.js
+++ script/snack-1.js
@@ -1,8 +1,12 @@
 // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
 
-let n = parseInt(prompt('Inserisci un numero compreso tra 1 e 10'));
 
-while (n < 1 || n > 10){
-    
+let i = 0;
+
+while ( (n < 1) || (n > 10) ){
     let n = parseInt(prompt('Inserisci un numero compreso tra 1 e 10'));
 }
+
+
+
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
