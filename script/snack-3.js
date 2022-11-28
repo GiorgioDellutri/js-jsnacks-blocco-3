1c21f674d1c9b97fc713111dd58f931dac0827fb
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-28 17:36:28 +0100

    v 2

--- script/snack-3.js
+++ script/snack-3.js
@@ -3,18 +3,13 @@
 
 let firstArray = [2,4,56,73,21,566,778,10,9,5];
 let secondArray = [2,4,56,73,21,566];
-let randomNumber = Math.floor(Math.random() * 101);
+let addNumber;
+console.log(firstArray);
+console.log(secondArray);
 
-while (firstArray.length === secondArray.length){
-
-    if (firstArray.length < secondArray.lenght){
-
-        firstArray.push(randomNumber);
-    } else if (secondArray.lenght < firstArray.length){
-        
-        secondArray.push(randomNumber);
-    }
-
+while(firstArray.length === secondArray.length){
+    addNumber = parseInt(prompt('Inserisci numero'));
+    secondArray.push(addNumber);
 }
 
 console.log(firstArray);
